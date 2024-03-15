c877ccd3787e60e60604412b079a6f38b40ff57a
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-03-15 13:30:16 +0900

    feat: recoil set selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useRecoilState} from "recoil";
+import {hourSelector, minuteState} from "./atoms";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [minutes, setMinutes] = useRecoilState(minuteState);
+    const [hours, setHours] = useRecoilState(hourSelector);
+    const onMinutesChage = (event: React.FormEvent<HTMLInputElement>) => {
+        setMinutes(+event.currentTarget.value);
+    };
+    const onHoursChage = (event: React.FormEvent<HTMLInputElement>) => {
+        setHours(+event.currentTarget.value);
+    };
+    return (
+        <div>
+            <input value={minutes} onChange={onMinutesChage} type="number" placeholder="Minutes"></input>
+            <input value={hours} onChange={onHoursChage} type="number" placeholder="Hours"></input>
+        </div>
+    );
 }
 
 export default App;
